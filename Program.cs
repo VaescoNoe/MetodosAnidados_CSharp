0b0a1fc6fb50c2881e149e62e2e1aeb4c5013008
Noe <VaescoNoe@gmail.com>
2019-04-23 23:23:06 -0500

    Metodos anidados

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        metodo();
+
+    }
+
+    static void metodo()
+    {
+        Console.WriteLine("Metodo padre");
+        metodoUno();
+
+        void metodoUno() {
+            Console.WriteLine("Metodo privado");
+        }
+    }
+}
